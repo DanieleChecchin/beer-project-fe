779c5f9643359a15b4f094c0763c8501c2461e10
DanieleChecchin <danielechecchin26@gmail.com>
2025-01-28 16:08:20 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+
+import { RouterView } from 'vue-router';
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+
+</script>
+
+<template>
+
+  <AppHeader />
+  <RouterView />
+  <AppFooter />
+
+</template>
+
+<style lang="scss">
+@use "bootstrap/scss/bootstrap.scss" as *;
+@use "./styles/general.scss" as *;
+</style>
---
+++ src/js/router.js
@@ -0,0 +1,16 @@
+import { createWebHistory, createRouter } from 'vue-router';
+
+import AppHome from '../pages/AppHome.vue';
+import AppAbout from '../pages/AppAbout.vue';
+
+const routes = [
+    { path: '/', component: AppHome, name: 'home' },
+    { path: '/about', component: AppAbout, name: 'about' },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export { router };
