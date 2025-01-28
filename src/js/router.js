37c5131932dbc5a0f009199da7e02f057a743d4f
Beniamino-Soccio <beniamino43@gmail.com>
2025-01-28 17:47:57 +0100

    hotfix router

--- src/js/router.js
+++ src/js/router.js
@@ -5,12 +5,10 @@ import AppAbout from '../pages/AppAbout.vue';
 import AppBeers from '../pages/AppBeers.vue';
 
 const routes = [
-    { path: '/', component: AppHome, name: 'home' },
+    { path: '/', redirect: '/beers' }, // Redirect automatico alla route "/beers"
     { path: '/about', component: AppAbout, name: 'about' },
     { path: '/beers', component: AppBeers, name: 'beers' },
-
 ];
-
 const router = createRouter({
     history: createWebHistory(),
     routes,
